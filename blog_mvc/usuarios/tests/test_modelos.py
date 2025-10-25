71b29bc69692ced501e2a76bb0ba7d879408f010
Halepan <alejandrohenry62@gmail.com>
2025-10-25 03:43:13 -0400

    todavia existen errores en el models para implemetnar lo mejor posible los usuarios

--- blog_mvc/usuarios/tests/test_modelos.py
+++ blog_mvc/usuarios/tests/test_modelos.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
 from usuarios.models import Users_model
 
-class testgara(TestCase):
+class testusuario(TestCase):
 
     def test_create_user(self):
         #se crea una instancia en la cual no de se guarda en la base de datos pero si es funcionable para testear si funcionara
-        userM = Users_model.objects.create_user(username="henry",email = "alejandrohenry@gmail.com", password="henry12345678")
+        userM = Users_model.objects.create(username="henry",email = "alejandrohenry@gmail.com", password="henry12345678")
 
         self.assertEqual(userM.username, "henry")
         self.assertEqual(userM.email,"alejandrohenry@gamil.com")
@@ -13,8 +13,8 @@ class testgara(TestCase):
 
     def test_have_email(self):
         #este test sirve para validar si genero un email en el usuario cuando se cree
-        userM = Users_model.objects.create_user(username="henry",email = "alejandrohenry@gmail.com", password="henry12345678")
+        userM = Users_model.objects.create(username="henry",email = "alejandrohenry@gmail.com", password="henry12345678")
 
         self.assertTrue(userM.email)
 
-        
+        """reestructura y vuelve a hacer el models para que funcione como un usuario normal"""
