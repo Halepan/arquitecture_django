72f0efa394815b826fc46c24b64aa9fe2f40b899
henry alejandro paniagua <alejandrohenry62@gmail.com>
2025-11-07 15:20:14 -0500

    se agrego el testUp para definir como trabajara el test

--- blog_mvc/usuarios/tests/test_view.py
+++ blog_mvc/usuarios/tests/test_view.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 from usuarios.views import Usuario_views
 
 class testviewsusers(TestCase):
+    def setUp(self):
+        return super().setUp()
+
     def test_views_exsit(self):
 
         view = Usuario_views()
